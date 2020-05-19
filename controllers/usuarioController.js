d7e7592838aeb9b303d376a0c8c4e1ab14a5efbc
Aitor Arina <aitorulz1@gmail.com>
2020-05-19 18:06:21 +0200

    reconoce la base de datos y primer usuario subido con postman y revisado con robot3

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,19 @@
-exports.crearUsuario = (req, res) => {
-    console.log('viendesde creando usuarios')
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario =  async (req, res) => {
+    try{
+        let usuario;
+
+        // crea el usuario
+        usuario = new Usuario(req.body) ;
+
+        // Guardar Usuario
+        await usuario.save();
+
+        // Mensaje de confirmación
+        res.send('Usuario creado correctamente');
+    }catch(error){
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // Primero importo express
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // Y luego utilizamos su función. Crea el servidor
 const app = express();
 
-// Puerto de la app. Calquier nº que no sea 3000. Al subir a heroku se espera que la variable de entorno se llame PORT.
-const PORT = process.env.PORT || 4000
+// Conectar a las BBDD
+conectarDB();
 
+// Habilitar  express.json
+app.use(express.json({extended: true}));
+
+// Puerto de la app. Calquier nº que no sea 3000. Al subir a heroku se espera que la variable de entorno se llame PORT.
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
-app.use('/api/usuarios', require('./routes/usuarios'))
+app.use('/api/usuarios', require('./routes/usuarios'));
 
 
 // Definir la página principal. Es solo para comprobar que funciona. Una vez que ves Hola Mundo puedes borrarlo.
 app.get('/', (req, res) => {
-    res.send('Hola mundo')
+    res.send('Hola mundo');
 })
 
 // Arrancar la app
 app.listen(PORT, () => {
-    console.log(`El servidor está funcionando en el ${PORT}`)
+    console.log(`El servidor está funcionando en el ${PORT}`);
 })
