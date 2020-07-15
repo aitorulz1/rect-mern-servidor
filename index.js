b0fb85857a48d1019c759098ccd25866a964f6bd
Aitor Arina <aitorulz1@gmail.com>
2020-07-15 20:11:42 +0200

    señalando el cambio en el archivo index.js

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+                                                app.use((req, res, next) => {
+                                                    res.header('Access-Control-Allow-Origin', '*');
 
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+                                                    // authorized headers for preflight requests
+                                                    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+                                                    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+                                                    next();
 
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
+                                                    app.options('*', (req, res) => {
+                                                        // allowed XHR methods  
+                                                        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+                                                        res.send();
+                                                    });
+                                                });
 
 // Puerto de la app. Calquier nº que no sea 3000. Al subir a heroku se espera que la variable de entorno se llame PORT.
 const port = process.env.port || 4000;
