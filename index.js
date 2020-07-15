ca06be8d83534b3b87af8d2c8900761a3d7f74b9
Aitor Arina <aitorulz1@gmail.com>
2020-07-15 20:06:58 +0200

    configurando cors para lograr autenticar al usuario

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ app.use(cors());
 // Habilitar  express.json
 app.use(express.json({extended: true}));
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 // Puerto de la app. Calquier nº que no sea 3000. Al subir a heroku se espera que la variable de entorno se llame PORT.
 const port = process.env.port || 4000;
 
